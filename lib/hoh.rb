f350161641f03c225b016f653af221673453a242
Joseph S. Cha <josephshincha@gmail.com>
2020-01-24 01:08:04 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -29,33 +29,9 @@
 #   label: "Species",
 #   sub_category: nil
 # }
-PORTION_1 = {
-  label: "Kingdom",
-  sub_category: {
-    label: "Phylum",
-    sub_category: {
-      label: "Class"
-    }
-  }
-}
 
-  PORTION_2 = {
-  label: "Order"
-}
-
-  PORTION_3 = {
-  label: "Family",
-  sub_category: {
-    label: "Genus",
-  }
-}
-
-  PORTION_4 = {
-  label: "Species",
-  sub_category: nil
-}
 def naming_system
-  taxonomy = [PORTION_1],[PORTION_2],[PORTION_3],[PORTION_4]
+  taxonomy = 
   # Remember:
   #  Kingdom
   #  Phylum
