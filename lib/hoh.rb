58fa101817bed76454b90f619c1f737e2fee7053
Joseph S. Cha <josephshincha@gmail.com>
2020-01-24 01:15:10 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@ PORTION_4 = {
 }
 
 def naming_system
-  taxonomy = [{PORTION_1},{PORTION_2},{PORTION_3},{PORTION_4}]
+  taxonomy = [PORTION_1,PORTION_2,PORTION_3,PORTION_4]
   # Remember:
   #  Kingdom
   #  Phylum
