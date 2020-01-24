2726df66d6aa8cf0d46722d818cd1c938d4717c8
Joseph S. Cha <josephshincha@gmail.com>
2020-01-24 01:31:02 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -52,5 +52,5 @@ PORTION_4 = {
 def naming_system
   PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
   PORTION_2[:sub_category] = PORTION_3
-  PORTION_3
+  PORTION_3[:sub_category][:sub_category] 
 end
